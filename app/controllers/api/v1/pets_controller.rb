ea8d1227da14f00fb65e756efed3fb175d0feda0
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2020-02-17 14:03:01 -0500

    delete toggle_adopt

--- app/controllers/api/v1/pets_controller.rb
+++ app/controllers/api/v1/pets_controller.rb
@@ -2,31 +2,24 @@ class Api::V1::PetsController < ApplicationController
 
 
 
-  def toggle_adopt
-    @pet = Pet.find(params[:id])
-		@pet.update(to_adopt: !@pet.to_adopt)
-		render json: @pet
-	end
 
 	def adopt
     @pet = Pet.find(params[:id])
 
     @pet.update(to_adopt: false, owner_id: curr_user.id)
-    
+
 		render json: @pet
+
 	end
 
 
-  # GET /@@pets
   def index
     @pets =  Pet.where(to_adopt: true)
-    # @@pets = Pet.all
     render json: @pets
   end
 
 
 
-  # PATCH/PUT /@pets/1
 
 
   end
