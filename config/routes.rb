579e2ef367a3a0b775cae3f6e162a621137e4a01
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2020-02-19 11:17:25 -0500

    changed home route for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,8 @@ Rails.application.routes.draw do
       post "/users/:id/get_pet", to: "users#get_pet"
 
       #
-
-      root :to => 'pets#index'
+      get '/', to: 'pets#index'
+      # root :to => 'pets#index'
 			post "/login", to: "auth#login"
 			get "/auto_login", to: "auth#auto_login"
 		end
