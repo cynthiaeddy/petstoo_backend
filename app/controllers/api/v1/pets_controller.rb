c738a7d4d34d4e45ce7405002a3281e3b091df82
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2020-02-18 20:17:57 -0500

    made no difference

--- app/controllers/api/v1/pets_controller.rb
+++ app/controllers/api/v1/pets_controller.rb
@@ -5,10 +5,16 @@ class Api::V1::PetsController < ApplicationController
 
 	def adopt
     @pet = Pet.find(params[:id])
+		# @owner = pet.owner
+		# @owner.update(owner.pet)
 
     @pet.update(to_adopt: false, owner_id: curr_user.id)
+		# curr_user.update(owner.pet)
 
 		render json: @pet
+
+
+
 
 	end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_14_161408) do
+ActiveRecord::Schema.define(version: 2020_02_19_010436) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2020_02_14_161408) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "balance"
   end
 
 end
