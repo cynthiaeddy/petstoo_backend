439ee02052e1d2a796ef535a0d86588278f46085
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2020-07-14 14:31:49 -0400

    not working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,9 @@
 class ApplicationController < ActionController::API
 
+ jwtToken = process.env.jwtToken
+
   def encode_token(user_id)
-    JWT.encode({user_id: user_id}, "12345")
+    JWT.encode({user_id: user_id}, jwtToken )
   end
   #
   def get_token
@@ -10,7 +12,7 @@ class ApplicationController < ActionController::API
   #
   def decode_token
     begin
-      JWT.decode(get_token, "12345")[0]["user_id"]
+      JWT.decode(get_token, jwtToken )[0]["user_id"]
     rescue
       # Token invalid, return nil
       nil
