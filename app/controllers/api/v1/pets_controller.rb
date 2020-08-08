21f48e4c28c8f514f8efe46825e6347e7e96e399
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2020-08-08 19:24:05 -0400

    sending to heroku

--- app/controllers/api/v1/pets_controller.rb
+++ app/controllers/api/v1/pets_controller.rb
@@ -1,11 +1,5 @@
 class Api::V1::PetsController < ApplicationController
-  # def toggle_adopt
-	# 	@pet = Pet.find(params[:id])
 
-	# 	@pet.update(to_adopt: !@pet.to_adopt)
-
-	# 	render json: @pet
-	# end
 
 
 
@@ -22,18 +16,7 @@ class Api::V1::PetsController < ApplicationController
 
 
   end
-  # def adopt
-  #   @pet = Pet.find(params[:id])
 
-
-  #   @pet.update(to_adopt: false, owner_id: curr_user.id)
-
-	# 	render json: @pet
-
-
-
-
-	# end
 
 
   def index
